87630b1af3871bab3d78b09e9818218f066745ce
ayush110 <67121244+ayush110@users.noreply.github.com>
2022-04-29 22:28:30 -0400

    Added-resume

--- components/Footer.js
+++ components/Footer.js
@@ -57,6 +57,11 @@ function Footer() {
         <div class="mr-3 hover:text-stone-400">
     <Link href="contact" ><a>contact</a></Link>
         </div>
+       
+        <div class="mr-3 text-stone-300">|</div>
+        <div class="mr-3 hover:text-stone-400">
+    <Link href="https://drive.google.com/file/d/1968twQ1dTwo8BmGl3VFQA9KtQb1folsv/view?usp=sharing" ><a>resume</a></Link>
+        </div>
     </div>
       
    
